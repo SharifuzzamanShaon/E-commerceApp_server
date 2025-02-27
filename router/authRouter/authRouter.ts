4f87674f993ea37db7659d895158f139fc4bec3a
sharifuzzamanShaon <shasan2200@gmail.com>
2025-02-27 20:59:36 +0600

    add login and update-token routes with validation; protect route with auth middleware

--- router/authRouter/authRouter.ts
+++ router/authRouter/authRouter.ts
@@ -1,6 +1,15 @@
 import { Router } from "express";
-import { register } from "../../controller/auth.controller";
+import { login, register, updateToken } from "../../controller/auth.controller";
+import { runValidation } from "../../middleware/validation";
+import { schema } from "../../middleware/validation/schema";
+import authMiddleware from "../../middleware/auth/authMiddleware";
+
 const router = Router();
 
-router.post("/register", register)
+router.post("/register", runValidation(schema.register), register)
+router.post("/login", runValidation(schema.login), login)
+router.get("/update-token", updateToken)
+router.get("/protected", authMiddleware, (req, res, next) => {
+    res.status(200).send({ message: "Protected route" })
+})
 export default router;
