6c48721057b44782c48f08fd14a02f7bc6e19b12
sharifuzzamanShaon <shasan2200@gmail.com>
2025-02-27 20:59:10 +0600

    refactor validation schemas to consolidate user registration and login into a single export

--- middleware/validation/schema.ts
+++ middleware/validation/schema.ts
@@ -1,11 +1,13 @@
 import Joi from "joi";
 
-export const userSchema = Joi.object({
-    name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-});
-export const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-});   
+export const schema ={
+    register: Joi.object({
+        name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).required(),
+      }),
+      login: Joi.object({
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).required(),
+      }),
+}
