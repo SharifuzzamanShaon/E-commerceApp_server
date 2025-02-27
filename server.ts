0ab3d9ad2e215b5c7d3ea1c8bb9b4fc14d4c4246
sharifuzzamanShaon <shasan2200@gmail.com>
2025-02-27 15:11:15 +0600

    initialize server with Express, add package.json and .gitignore

---
+++ server.ts
@@ -0,0 +1,37 @@
+import {Express, NextFunction, Request, Response } from "express";
+import express from 'express';
+const app:Express = express();
+const port = process.env.PORT || 5005;
+import router from "./router";
+import bodyParser from "body-parser";
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+
+
+
+//Test router
+app.get('/', (_req:Request, res:Response) => {
+    res.status(200).send({message : 'Welcome to the server'});
+})
+
+
+app.use("/api/v1", router);
+
+interface CustomError extends Error {
+    statusCode?: number;
+    message: string;
+}
+
+app.use((err: CustomError, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).send({ message });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on  http://localhost:${port}`);
+});
