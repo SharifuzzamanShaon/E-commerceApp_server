d6dd028c571c406510a522ab7dcf942571b837d2
sharifuzzamanshaon <shasan2200@gmail.com>
2025-03-11 13:02:35 +0600

    add product router to main application routes

--- router/index.ts
+++ router/index.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import register from "./authRouter/authRouter";
+import productRouter from "./productRouter/productRouter";
 const router = Router();
 
 router.use("/auth", register)
-
+router.use("/product", productRouter)
 export default router;
