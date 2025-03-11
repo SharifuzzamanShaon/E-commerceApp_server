cf9def3dd05b03fe82023b36116982c3c27b2282
sharifuzzamanshaon <shasan2200@gmail.com>
2025-03-11 13:01:58 +0600

    add validation schemas for product and category management

--- middleware/validation/schema.ts
+++ middleware/validation/schema.ts
@@ -1,13 +1,29 @@
 import Joi from "joi";
 
-export const schema ={
-    register: Joi.object({
-        name: Joi.string().required(),
-        email: Joi.string().email().required(),
-        password: Joi.string().min(6).required(),
-      }),
-      login: Joi.object({
-        email: Joi.string().email().required(),
-        password: Joi.string().min(6).required(),
-      }),
+export const schema = {
+  register: Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+  }),
+  login: Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+  }),
+  product: Joi.object({
+    name: Joi.string().required(),
+    price: Joi.number().required(),
+    description: Joi.string().required(),
+    image: Joi.string().optional(),
+    categoryId: Joi.string().required(),
+    stock: Joi.number().required(),
+
+  }),
+  category:Joi.object({
+    name:Joi.string().required()
+  }),
+  subCategory:Joi.object({
+    name:Joi.string().required(),
+    parentCategoryId:Joi.string().required()
+  })
 }
