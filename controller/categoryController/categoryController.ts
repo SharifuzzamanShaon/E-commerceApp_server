443891faf48aedef8412026a5172a5be0b139a30
sharifuzzamanshaon <shasan2200@gmail.com>
2025-03-12 15:22:43 +0600

    refactor categoryController for subcategory creation and retrieval logic

--- controller/categoryController/categoryController.ts
+++ controller/categoryController/categoryController.ts
@@ -15,7 +15,7 @@ const createCategory = async (req: Request, res: Response, next: NextFunction) =
         next(error)
     }
 }
-const addSubcategoy = async (req: Request, res: Response, next: NextFunction):Promise<void> => {
+const addSubcategoy = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
         const { name, parentCategoryId } = req.body;
         const isCategoryExist = await prisma.subCategroy.findUnique({
@@ -24,24 +24,59 @@ const addSubcategoy = async (req: Request, res: Response, next: NextFunction):Pr
                 parentCategory: parentCategoryId
             }
         })
-        if (isCategoryExist) {
-             res.status(400).send({ message: `${name} already exist` })
+        const isCategoryExist_l2 = await prisma.subSubCategory.findUnique({
+            where: {
+                name,
+                parentCategory: parentCategoryId
+            }
+        })
+        if (isCategoryExist || isCategoryExist_l2) {
+            res.status(400).send({ message: `${name} already exist` })
         } else {
-            const category = await prisma.subCategroy.create({
-                data: {
-                    name,
-                    parentCategory: parentCategoryId,
-                }
-            })
-            res.status(201).send({ message: `${category.name} added successfully` })
+            let category
+            try {
+                category = await prisma.subCategroy.create({
+                    data: {
+                        name,
+                        parentCategory: parentCategoryId,
+                    }
+                });
+                res.status(201).send({ message: `${category.name} subCategory added successfully` });
+            } catch (error) {
+                category = await prisma.subSubCategory.create({
+                    data: {
+                        name,
+                        parentCategory: parentCategoryId,
+                    }
+                });
+                res.status(201).send({ message: `${category.name} subSubCategory added successfully` });
+            }
         }
     } catch (error) {
         next(error)
     }
 }
+
 const getAllCategories = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const categories = await prisma.category.findMany();
+        const categories = await prisma.category.findMany({
+            select: {
+                id: true,
+                name: true,
+                subCategories: {
+                    select: {
+                        id: true,
+                        name: true,
+                        SubSubCategory: {
+                            select: {
+                                id: true,
+                                name: true
+                            }
+                        }
+                    }
+                }
+            }
+        });
         res.status(200).send(categories)
     } catch (error) {
         next(error)
