b1ebd1204d32df3135e9d40490c49667efe2b160
sharifuzzamanshaon <shasan2200@gmail.com>
2025-04-29 18:09:52 +0600

    feat: add routes for product retrieval by category and new arrivals

--- router/productRouter/productRouter.ts
+++ router/productRouter/productRouter.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
-import { getAllProducts, createProduct } from "../../controller/productController/productController";
+import { getAllProducts, createProduct, getNewArrivals, getProductByCategory } from "../../controller/productController/productController";
 import { runValidation } from "../../middleware/validation";
 import { schema } from "../../middleware/validation/schema";
 import { addSubcategoy, createCategory, getAllCategories } from "../../controller/categoryController/categoryController";
 const router = Router();
 
 router.get("/", getAllProducts);
+router.get("/product-by-category/:categoryId", getProductByCategory);
 router.post("/", runValidation(schema.product),  createProduct);
 router.post("/create-category", runValidation(schema.category),  createCategory);
 router.post("/create-sub-category", runValidation(schema.subCategory),  addSubcategoy);
 router.get("/all-categories", getAllCategories);
+router.get("/new-arrivals", getNewArrivals);
 export default router;
