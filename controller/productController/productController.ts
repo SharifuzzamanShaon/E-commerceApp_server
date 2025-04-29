4f458a76672057bf1580e108224ff7dabdac128c
sharifuzzamanshaon <shasan2200@gmail.com>
2025-04-29 18:09:14 +0600

    refactor: enhance product retrieval and creation logic in productController

--- controller/productController/productController.ts
+++ controller/productController/productController.ts
@@ -14,27 +14,77 @@ const getAllProducts = async (req: Request, res: Response) => {
             .send({ message: "Error while fetching products", error });
     }
 };
+const getProductByCategory = async (req: Request, res: Response) => {
+    try {
+        const { categoryId } = req.params;
+        const products = await prisma.product.findMany({
+            where: {    
+                subSubCategory: {
+                    id: categoryId
+                }, 
+                OR: [
+                    {
+                        subCategory: {
+                            id: categoryId
+                        }
+                    },
+                    {
+                        subSubCategory: {
+                            id: categoryId
+                        }
+                    }
+                ]
+            }
+        });
+        res.status(200).send(products);
+    } catch (error) {
+        res
+            .status(500)
+            .send({ message: "Error while fetching products", error });
+    }
+}
+const getNewArrivals = async (req: Request, res: Response) => {
+    try {
+        const products = await prisma.product.findMany({
+            where: {
+                stock: {
+                    gte: 10
+                }
+            },
+            orderBy: {
+                createdAt: 'desc' // newest first
+            },
+            take: 10 // limit to 10 products
+        });
+        res.status(200).send(products);
+    } catch (error) {
+        res
+            .status(500)
+            .send({ message: "Error while fetching products", error });
+    }
+}
 const createProduct = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const { name, price, description, image, categoryId, stock } = req.body;
+        const { name, price, description, image, categoryId, stock, color, size } = req.body;
         if (image) {
             const base64Image = image
             const buffer = Buffer.from(base64Image, "base64");
             fs.writeFileSync("outputfile", buffer);
-            let result = await uploadOnCloudinary(base64Image);
-            console.log(result);
-            let imageUrl;
-            if (result) {
-                imageUrl = result.secure_url;
+            const result = await uploadOnCloudinary(base64Image);
+            console.log(result?.secure_url);
+            if (result?.secure_url) {
+                const data = {
+                    name,
+                    price,
+                    imageUrl: result.secure_url,
+                    description,
+                    subSubCategoryId: categoryId,
+                    stock,
+                    color,
+                    size,
+                }
                 const product = await prisma.product.create({
-                    data: {
-                        name,
-                        price,
-                        imageUrl: result.secure_url,
-                        description,
-                        categoryId,
-                        stock,
-                    },
+                    data,
                 });
                 res.status(201).send(product);
             } else {
@@ -47,8 +97,10 @@ const createProduct = async (req: Request, res: Response, next: NextFunction) =>
                 price,
                 imageUrl: "",
                 description,
-                categoryId,
+                subSubCategoryId: categoryId,
                 stock,
+                color,
+                size,
             },
         });
         res.status(201).send(product);
@@ -56,4 +108,4 @@ const createProduct = async (req: Request, res: Response, next: NextFunction) =>
         next(error)
     }
 };
-export { getAllProducts, createProduct };
+export { getAllProducts, getProductByCategory, createProduct, getNewArrivals };
